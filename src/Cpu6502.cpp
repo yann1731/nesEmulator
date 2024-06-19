c248680089ae38d92b0d29674afd288195a4110a
fenix144 <yan_1731@hotmail.com>
2024-06-19 10:44:05 -0400

    Finish disassemble

--- src/Cpu6502.cpp
+++ src/Cpu6502.cpp
@@ -70,6 +70,7 @@ License (OLC-3)
 
 #include "../include/Cpu6502.hpp"
 #include "../include/Bus.hpp"
+#include <_types/_uint16_t.h>
 #include <cstdint>
 #include <sys/types.h>
 
@@ -867,7 +868,74 @@ std::map<uint16_t, std::string> Cpu6502::disassemble(uint16_t nStart, uint16_t n
             sInst += "IMM";
         }
         else if (lookup[opCode].addrmode == &Cpu6502::IMM) {
-
+            value = bus->read(addr, true);
+            addr++;
+            sInst += "#$" + hex(value, 2) + " {IMM}";
         }
+        else if (lookup[opcode].addrmode == &Cpu6502::ZP0) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = 0x00;
+            sInst += "$" + hex(lo, 2) + " {ZP0}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::ZPX) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = 0x00;
+            sInst += "$" + hex(lo, 2) + ", X {ZPX}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::ZPY) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = 0x00;
+            sInst += "$" + hex(lo, 2) + ", Y {ZPY}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::IIX) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = 0x00;
+            sInst += "($" + hex(lo, 2) + ", X) {IIX}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::IIY) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = 0x00;
+            sInst += "($" + hex(lo, 2) + ", Y) {IIY}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::ABS) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = bus->read(addr, true);
+            addr++;
+            sInst += "$" + hex((uint16_t) (hi << 8) | lo, 4) + " {ABS}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::ABX) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = bus->read(addr, true);
+            addr++;
+            sInst += "$" + hex((uint16_t) (hi << 8) | lo, 4) + ", X {ABX}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::ABY) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = bus->read(addr, true);
+            addr++;
+            sInst += "$" + hex((uint16_t) (hi << 8) | lo, 4) + ", Y {ABY}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::IND) {
+            lo = bus->read(addr, true);
+            addr++;
+            hi = bus->read(addr, true);
+            addr++;
+            sInst += "($" + hex((uint16_t) (hi << 8) | lo, 4) + ") {IND}";
+        }
+        else if (lookup[opcode].addrmode == &Cpu6502::REL) {
+            value = bus->read(addr, true);
+            addr++;
+            sInst += "$" + hex(value, 2) + " [$" + hex(addr + value, 4) + "] {REL}";
+        }
+        mapLines[addr] = sInst;
     }
+    return mapLines;
 }
