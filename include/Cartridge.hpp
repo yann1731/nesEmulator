3bd323dba3af5333b26507f854076c937ae257c9
fenix144 <yan_1731@hotmail.com>
2024-08-08 00:20:18 -0400

    Implement read/write cart functions

--- include/Cartridge.hpp
+++ include/Cartridge.hpp
@@ -17,16 +17,16 @@ public:
 	Cartridge(const std::string& cartName);
 	~Cartridge();
 
-	bool cpuRead(uint16_t addr, uint8_t data);
-	bool cpuWrite(uint16_t addr, uint8_t &data);
+	bool cpuRead(uint16_t addr, uint8_t &data);
+	bool cpuWrite(uint16_t addr, uint8_t data);
 
-	bool ppuRead(uint16_t addr, uint8_t data);
-	bool ppuWrite(uint16_t addr, uint8_t &data);
+	bool ppuRead(uint16_t addr, uint8_t &data);
+	bool ppuWrite(uint16_t addr, uint8_t data);
 private:
 	std::vector<uint8_t> v_prg_mem_;
 	std::vector<uint8_t> v_chr_mem_;
 	
-	InesHeader header;
+	InesHeader header_;
 
 	uint8_t n_mapper_id_;
 	uint8_t n_prg_banks_;
--- src/Cartridge.cpp
+++ src/Cartridge.cpp
@@ -1,21 +1,23 @@
 #include "../include/Cartridge.hpp"
 #include "../include/InesHeader.hpp"
+#include <_types/_uint32_t.h>
+#include <memory>
 
 Cartridge::Cartridge(const std::string& cartName): v_prg_mem_(0),
 v_chr_mem_(0),
 n_mapper_id_(0),
 n_prg_banks_(0),
 n_chr_banks(0) {
-	memset(&header, 0, sizeof(header));
+	memset(&header_, 0, sizeof(header_));
 
 	std::ifstream file;
 	file.open(cartName, std::ifstream::binary);
 	if (file.is_open()) {
-		file.read((char*) &header, sizeof(header));
+		file.read((char*) &header_, sizeof(header_));
 
-		if (header.mapper1 & 0x04)
+		if (header_.mapper1 & 0x04)
 			file.seekg(512, std::ios_base::cur);
-		n_mapper_id_ = ((header.mapper2 >> 4) << 4) | (header.mapper1 >> 4);
+		n_mapper_id_ = ((header_.mapper2 >> 4) << 4) | (header_.mapper1 >> 4);
 
 		uint8_t n_file_type = 1;
 		if (n_file_type == 0) {
@@ -23,17 +25,23 @@ n_chr_banks(0) {
 		}
 
 		if (n_file_type == 1) {
-			n_prg_banks_ = header.prg_rom_chunks;
+			n_prg_banks_ = header_.prg_rom_chunks;
 			v_prg_mem_.resize(n_prg_banks_ * 16384);
 			file.read((char *)v_prg_mem_.data(), v_prg_mem_.size());
 
-			n_chr_banks = header.chr_rom_chunks;
+			n_chr_banks = header_.chr_rom_chunks;
 			v_chr_mem_.resize(n_chr_banks * 8192);
 			file.read((char *)v_chr_mem_.data(), v_chr_mem_.size());
 		}
 
 		if (n_file_type == 2) {
 
+		}
+
+		switch (n_mapper_id_) {
+			case 0:
+				mapper_ = std::make_shared<Mapper000>(n_prg_banks_, n_chr_banks);
+				break;
 		}
 
 		file.close();
@@ -47,18 +55,46 @@ Cartridge::~Cartridge() {
 
 }
 
-bool Cartridge::cpuRead(uint16_t addr, uint8_t data) {
+bool Cartridge::cpuRead(uint16_t addr, uint8_t &data) {
+	uint32_t mapped_addr = 0;
 
+	if (mapper_->cpuMapRead(addr, mapped_addr)) {
+		data = v_prg_mem_[mapped_addr];
+		return true;
+	}
+	else
+		return false;
 }
 
-bool Cartridge::cpuWrite(uint16_t addr, uint8_t &data) {
+bool Cartridge::cpuWrite(uint16_t addr, uint8_t data) {
+	uint32_t mapped_addr = 0;
 
+	if (mapper_->cpuMapWrite(addr, mapped_addr)) {
+		v_prg_mem_[mapped_addr] = data;
+		return true;
+	}
+	else
+		return false;
 }
 
-bool Cartridge::ppuRead(uint16_t addr, uint8_t data) {
+bool Cartridge::ppuRead(uint16_t addr, uint8_t &data) {
+	uint32_t mapped_addr = 0;
 
+	if (mapper_->ppuMapRead(addr, mapped_addr)) {
+		data = v_chr_mem_[mapped_addr];
+		return true;
+	}
+	else
+		return false;
 }
 
-bool Cartridge::ppuWrite(uint16_t addr, uint8_t &data) {
+bool Cartridge::ppuWrite(uint16_t addr, uint8_t data) {
+	uint32_t mapped_addr = 0;
 
+	if (mapper_->ppuMapWrite(addr, mapped_addr)) {
+		v_chr_mem_[mapped_addr] = data;
+		return true;
+	}
+	else
+		return false;
 }
